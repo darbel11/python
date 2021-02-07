37914dacbcf32960f1f527ee27d01decde759c19
darbel11 <71098229+darbel11@users.noreply.github.com>
2021-02-08 00:12:58 +0300

    Fixed simplicity_check.py

--- simplicity_check.py
+++ simplicity_check.py
@@ -1,8 +1,17 @@
-a = int(input())
+import random
 
-def prime(a):
-    b = 3
-    while a % b != 0:
-        b+=1
-    return a == b
-print (prime(a))
+def is_int(str):
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
+
+N = input('Введите число, которое нужно проверить на простоту: ')
+
+if int (N) >= 0 and is_int('{}'.format(N)) is True:
+    a = random.randint(1,int(N)-1)
+    if (pow(a,int(N)-1,mod = int(N)) == 1):
+        print ('Число является простым')
+    else:
+        print ('Число не является простым')
