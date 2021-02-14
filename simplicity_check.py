36819270d496a5fddae0cc5aa0789f4382875a45
darbel11 <71098229+darbel11@users.noreply.github.com>
2021-02-15 00:12:57 +0300

    Refactored simplicity_check.py

--- simplicity_check.py
+++ simplicity_check.py
@@ -1,17 +1,15 @@
 import random
 
-def is_int(str):
-    try:
-        int(str)
-        return True
-    except ValueError:
-        return False
 
-N = input('Введите число, которое нужно проверить на простоту: ')
+def checking(n: int) -> bool:
+    if n >= 0:
+        a = random.randint(1, int(n) - 1)
+        if pow(a, int(n) - 1, mod=int(n)) == 1:
+            # print('Число является простым')
+            return True
+        else:
+            # print('Число не является простым')
+            return False
 
-if int (N) >= 0 and is_int('{}'.format(N)) is True:
-    a = random.randint(1,int(N)-1)
-    if (pow(a,int(N)-1,mod = int(N)) == 1):
-        print ('Число является простым')
-    else:
-        print ('Число не является простым')
+
+checking(int(input('Введите число, которое нужно проверить на простоту: ')))
