6c2e0a9a37ef3239cdf934b464652cae257427de
darbel11 <71098229+darbel11@users.noreply.github.com>
2021-10-01 18:59:22 +0700

    Refactored parallelize.py

--- parallelize.py
+++ parallelize.py
@@ -2,19 +2,23 @@ import multiprocessing
 import random
 import time
 
+
 def point_generation():
     generator = random.Random()
     x, y = generator.random(), generator.random()
 
-    return (x, y)
+    return x, y
+
 
 def point_generation_wrapper(_):
     return point_generation()
+
 
 def checker(point):
     x, y = point
     if x ** 2 + y ** 2 <= 1:
         return 1
+
 
 if __name__ == '__main__':
     MESSAGE = 'Введите число (точность вычислений будет зависеть от этого числа): '
@@ -23,7 +27,7 @@ if __name__ == '__main__':
     start_time = time.time()
 
     with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
-        points = pool.map(func=point_generation_wrapper, iterable=[0]*amount_of_points)
+        points = pool.map(func=point_generation_wrapper, iterable=[0] * amount_of_points)
         check_results = pool.map(func=checker, iterable=points)
 
     points_inside_the_area = 0
@@ -31,9 +35,9 @@ if __name__ == '__main__':
         if result:
             points_inside_the_area += 1
 
-    calculated_pi = points_inside_the_area/amount_of_points * 4
+    calculated_pi = points_inside_the_area / amount_of_points * 4
 
-    print('Calculeted value of PI:', calculated_pi, sep=' ')
+    print('Calculated value of PI:', calculated_pi, sep=' ')
     print('Operating time: ', time.time() - start_time, sep=' ')
 
 else:
