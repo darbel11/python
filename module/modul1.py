d4f740660c9c7c8f154a74e1917bada82dfd255c
darbel11 <71098229+darbel11@users.noreply.github.com>
2021-10-06 21:04:03 +0700

    Added modul1.py

---
+++ module/modul1.py
@@ -0,0 +1,7 @@
+user_input = int(input('Введите значение: '))
+simplicity = is_number_prime(user_input)
+
+if simplicity:
+    print('Введенное число {} простое'.format(user_input))
+else:
+    print('Введенное число {} составное'.format(user_input))
